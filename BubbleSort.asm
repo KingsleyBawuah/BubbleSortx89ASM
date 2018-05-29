02a07a5700a644cd8a7b4a54881485d395fea335
Kingsley Bawuah <kingsley404@gmail.com>
2018-05-28 23:04:17 -0400

    Implemented random number generation, arrayFill, display array func, and began median implementation (DOES NOT COMPILE)

--- BubbleSort.asm
+++ BubbleSort.asm
@@ -12,6 +12,7 @@ INCLUDE Irvine32.inc
 	MAX						=		200
 	LO						=		100
 	HI						=		999
+	TWO						=		2
 
 .data
 	; declare variables here
@@ -20,6 +21,8 @@ INCLUDE Irvine32.inc
 	instruction				BYTE	"Please enter a number between 10 and 200, and i will generate random integers in a range of 100 - 999. I will then sort the numbers in descending order, display them and the median value which will be rounded to the nearest integer",0
 	unsortText				BYTE	"The unsorted numbers are : ",0
 	sortText				BYTE	"The sorted numbers are : ",0
+	medianText				BYTE	"The median of the list of generated numbers is: ",0
+	wordSpace				BYTE	"   ",0
 	request					DWORD	?
 	array					DWORD	MAX		DUP(?)
 
@@ -34,6 +37,12 @@ main proc
 	PUSH		OFFSET array
 	PUSH		request
 	CALL		fillArray
+	PUSH		OFFSET array
+	PUSH		request
+	CALL		displayList
+	PUSH		OFFSET array
+	PUSH		request
+	CALL		displayMedian
 
 	invoke ExitProcess,0
 main endp
@@ -99,9 +108,7 @@ fillArray	 PROC
 		MOV			EAX,900
 		CALL		RandomRange
 		ADD			EAX,LO
-		MOV			EDI,EAX ;Moves random int from EAX into EDI referenced as value.
-		CALL		WriteDec ;TESTING random number generation.
-		CALL		CrlF;TESTING
+		MOV			[EDI],EAX ;Moves random int from EAX into EDI referenced as value.
 
 
 		;Move through each element
@@ -110,7 +117,7 @@ fillArray	 PROC
 		LOOP		continue
 
 	POP			EBP
-	RET			8
+	RET			
 
 
 
@@ -128,22 +135,77 @@ sortList	 ENDP
 
 displayMedian PROC
 
+
+
+
+
 ;Procedure starter kit
 	PUSH		EBP
 	MOV			EBP,ESP
+
+
+;Then
+	MOV			EDI,[EBP+12] ;Move array parameter to the EDI register.
+	MOV			ECX,[EBP+8] ;Bring the counter in to the ECX register.
+
+;Divide request in half to find median.
+
+	MOV		EAX, ECX
+	CDQ
+	MOV		EDX,2
+	CDQ		;This is causing 0 to be placed in EDX. why?
+	DIV		EDX
+
+	POP		EBP
+	RET		
+
 
 displayMedian ENDP
 
-;Procedure starter kit
-	PUSH		EBP
-	MOV			EBP,ESP
-
 displayList	  PROC
 
+
+
 ;Procedure starter kit
 	PUSH		EBP
 	MOV			EBP,ESP
 
+
+;Then
+	MOV			EDI,[EBP+12] ;Move array parameter to the EDI register.
+	MOV			ECX,[EBP+8] ;Bring the counter in to the ECX register
+	MOV			EBX,9 ;Move line counter to EBX
+
+
+		continue:
+		MOV			EAX,[EDI] ;Moves random int from EAX into EDI referenced as value.
+		CALL		WriteDec ;TESTING array is filled.
+		MOV			EDX,OFFSET wordSpace
+		CALL		WriteString
+
+		CMP			EBX,0
+		JE			nextline
+		JG			nextElement
+
+
+		nextLine:
+		CALL		CrlF
+		MOV			EBX,10
+		JMP			nextElement
+
+
+		;Move through each element
+		nextElement:
+		DEC			EBX
+		ADD			EDI,4
+		LOOP		continue
+
+
+		
+
+	POP			EBP
+	RET			8
+			
 displayList	  ENDP
 
 
